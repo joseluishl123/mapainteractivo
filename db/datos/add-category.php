39bf68438b0919cbcbcab167a30183fd962cc0bf
JOSELO <JOSELO@DESKTOP-26KMVBP>
2021-04-13 15:13:43 -0500

    Conexion

---
+++ db/datos/add-category.php
@@ -0,0 +1,25 @@
+<?php
+  include('../conexion/dbconnection.php');
+  $data = json_decode(file_get_contents('php://input') );
+$nombre = $data['nombre'];
+$descripcion = $data['descripcion'] ;
+$latitud = $data['latitud '] ;
+$longitud = $data['longitud'] ;
+$imagen = $data['imagen'] ;
+
+
+$sql="INSERT INTO `logar`( `nombre`, `descripcion`, `fecha`, `latitud`, `longitud`, `imagen`, `estadoid`) 
+VALUES (:nombre,:descripcion,NOW(),:latitud,:longitud,:imagen,1)";
+$query=$dbh->prepare($sql);
+
+$query->bindParam(':nombre',$nombre);
+$query->bindParam(':descripcion',$descripcion);
+$query->bindParam(':latitud',$latitud);
+$query->bindParam(':longitud',$longitud);
+$query->bindParam(':imagen',$imagen);
+
+ $query->execute();
+ $retornar["respuesta"]= "datos insertados";
+ $retornar["recibe"]= $data;
+ echo json_encode($data)
+ ?>
---
+++ db/datos/lista.php
@@ -0,0 +1,18 @@
+<?php
+include('../conexion/dbconnection.php');
+// establecer y realizar consulta. guardamos en variable.
+$sql = "SELECT * FROM logar";
+$query = $dbh->prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+$array = array();
+$x=0;
+foreach($results as $row){
+    $array[$x] = $row;
+    $x++;
+}
+$retornar["recibe"]= $array;
+echo json_encode($retornar);
+
+?>
